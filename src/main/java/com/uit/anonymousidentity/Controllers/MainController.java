1f33723b54753374e634bf98546b206862d96ac1
NguyenDuyY <duyy.uit@gmail.com>
2016-10-06 10:44:03 +0700

    new Controller

--- src/main/java/com/uit/anonymousidentity/Controllers/MainController.java
+++ src/main/java/com/uit/anonymousidentity/Controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.uit.anonymousidentity.Controllers;
 
+
 import com.uit.anonymousidentity.Models.Authenticator;
 import java.math.BigInteger;
 import java.security.SecureRandom;
@@ -22,7 +23,13 @@ import com.uit.anonymousidentity.Models.Issuer.IssuerPublicKey;
 import com.uit.anonymousidentity.Models.Issuer.IssuerSecretKey;
 import com.uit.anonymousidentity.Models.Issuer.JoinMessage1;
 import com.uit.anonymousidentity.Models.Issuer.JoinMessage2;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+import javax.servlet.http.HttpServletResponse;
 @Controller
 public class MainController {
 	private BNCurve curve;
@@ -31,6 +38,14 @@ public class MainController {
 	private final String TPM_ECC_BN_P256 = "TPM_ECC_BN_P256";
         private final String JSON = "JSON";
         private SecureRandom random;
+        public final String SIG = "sig",
+                KRD = "krd",
+                APPID = "appId",
+                JoinMessage1 = "JoinMessage1",
+                SID = "sid",
+                JSID = "jsid",
+                SSID = "ssid",
+                   MSG = "msg",OK ="ok", ERROR = "error", NONCE ="nonce", DATA ="data";
 	
 	public MainController() throws NoSuchAlgorithmException{
 		curve = new BNCurve(BNCurveInstantiation.valueOf(TPM_ECC_BN_P256));
@@ -44,19 +59,48 @@ public class MainController {
             
         }
         //recieve sid from host and produce none to host to generate join messgae 1
-	@RequestMapping(value = "/getNonce" , method = RequestMethod.GET)
-	public ModelAndView SendNonce(){
-		
+	@RequestMapping(value = "/getNonce" , method = RequestMethod.POST)
+	public void SendNonce(HttpServletRequest request ,  HttpServletResponse response) throws IOException{
+                String sid = request.getParameter(SID);
+                String jsid = request.getParameter(JSID);
+                	
 		BigInteger n = new BigInteger(130,random);
-		return new ModelAndView("json","json", n );
+                //save to database
+                //response to client 
+                response.setStatus(200);
+                String json ;
+                
+
+                if(sid != null && jsid != null ){
+                    json = "{" + MSG + ":" + OK +"," + NONCE +":"+n.toString()+"}";
+                }
+                else {
+                    json = "{" + MSG + ":" + ERROR + "}";
+                }
+                PrintWriter out = response.getWriter();
+                out.println(json);
+                
+		
 	}
         //recive join message 1 and produce join message 2 
         @RequestMapping(value = "/getJoinMessage2",method = RequestMethod.POST)
-        public ModelAndView sendJoinMessage2(HttpServletRequest request ) throws NoSuchAlgorithmException{
-            String json  = request.getParameter(JSON);
+        public void sendJoinMessage2(HttpServletRequest request , HttpServletResponse response) throws NoSuchAlgorithmException, IOException{
+            String json  = request.getParameter(DATA);
             JoinMessage1 jm1 = new JoinMessage1(curve, json);
             JoinMessage2 jm2 = issuer.EcDaaIssuerJoin(jm1);
-            return new ModelAndView("json","json", jm2);
+            //response
+            response.setStatus(200);
+            String res;
+            if(jm2 == null){
+                res = "{" + MSG + ":" + "Invalid join message"+"}";
+            }
+            else{
+                res = "{" + MSG + ":" +OK + ","+ 
+                        DATA + ":" + jm2.toJson(curve) + "}";
+                        
+            }
+            PrintWriter out = response.getWriter();
+            out.println(res);
         }
         @RequestMapping(value = "/verify", method = RequestMethod.POST)
         public ModelAndView Verify(HttpServletRequest request ) throws NoSuchAlgorithmException {
